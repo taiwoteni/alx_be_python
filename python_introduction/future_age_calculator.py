776c5e87d475303572885985a4e0b930ced921fe
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-05-23 18:49:39 +0100

    ...

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+current_age = int(input("How old are you? "))
+
+# Calculate the age in 2050
+years_to_add = 2050 - 2023
+age_in_2050 = current_age + years_to_add
+
+# Display the result
+print('In 2050, you will be ' age_in_2050, ' years old.')
