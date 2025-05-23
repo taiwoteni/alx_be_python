054182a849a5772a967e99a1b5470a9bfe4a4aa4
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-05-23 18:50:35 +0100

    ...

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -5,4 +5,4 @@ years_to_add = 2050 - 2023
 age_in_2050 = current_age + years_to_add
 
 # Display the result
-print('In 2050, you will be ' age_in_2050, ' years old.')
+print('In 2050, you will be ', age_in_2050, ' years old.')
